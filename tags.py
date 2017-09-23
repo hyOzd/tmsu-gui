45c0b839b387165d062e25408fbe64856e149bc2
Hasan Yavuz ÖZDERYA <hy@ozderya.net>
2017-09-23 19:03:32 +0300

    file tagging/untagging implemented

--- tags.py
+++ tags.py
@@ -31,6 +31,22 @@ class Tmsu:
             return r.split(':')[1].split()
         return self._cmd('tags').splitlines()
 
+    def tag(self, fileName, tagName):
+        try:
+            self._cmd('tag {} {}'.format(fileName, tagName))
+            return True
+        except sp.CalledProcessError as e:
+            print("Failed to tag file.")
+            return False
+
+    def untag(self, fileName, tagName):
+        try:
+            self._cmd('untag {} {}'.format(fileName, tagName))
+            return True
+        except sp.CalledProcessError as e:
+            print("Failed to untag file.")
+            return False
+
     def _cmd(self, cmd):
         return sp.check_output('tmsu ' + cmd, shell=True).decode('utf-8')
 
@@ -44,19 +60,28 @@ class Tmsu:
             return None
 
 class MyWindow(Gtk.Window):
-    def __init__(self):
+    def __init__(self, tmsu, fileName):
         Gtk.Window.__init__(self, title="Tags")
-        self.set_size_request(300, 400)
 
+        self.tmsu = tmsu
+        self.fileName = fileName
+
+        self.set_size_request(300, 400)
         self.vbox = Gtk.Box(parent = self,
                             orientation = Gtk.Orientation.VERTICAL)
+        self.store = Gtk.ListStore(str, bool)
 
-        self.store = Gtk.ListStore(str, bool) # tag
+
+        # tag name column
         self.list_widget = Gtk.TreeView(self.store)
-        col = Gtk.TreeViewColumn("Tags", Gtk.CellRendererText(), text=0)
+        col = Gtk.TreeViewColumn("Tags", Gtk.CellRendererText(editable=True), text=0)
         col.set_expand(True)
         self.list_widget.append_column(col)
-        col = Gtk.TreeViewColumn("Checked", Gtk.CellRendererToggle(), active=1)
+
+        # 'tagged' checkbox column
+        cell = Gtk.CellRendererToggle()
+        cell.connect("toggled", self.on_cell_toggled)
+        col = Gtk.TreeViewColumn("Checked", cell, active=1)
         self.list_widget.append_column(col)
         self.vbox.pack_start(self.list_widget, True, True, 0)
 
@@ -67,11 +92,43 @@ class MyWindow(Gtk.Window):
         hbox.pack_end(self.add_button, False, False, 0)
         self.vbox.pack_end(hbox, False, False, 0)
 
-        # test data
-        self.store.append(["tag", True])
-        self.store.append(["tagm", True])
-        self.store.append(["dfdf", True])
-        self.store.append(["kkasd", False])
+        self.loadTags()
+
+    def on_cell_toggled(self, widget, path):
+        tagName = self.store[path][0]
+        isTagged = self.store[path][1]
+        if not isTagged:
+            r = self.tagFile(tagName)
+        else:
+            r = self.untagFile(tagName)
+
+        # toggle
+        if r: self.store[path][1] = not self.store[path][1]
+
+    def tagFile(self, tagName):
+        if not self.tmsu.tag(self.fileName, tagName):
+            self.displayError("Failed to tag file.")
+            return False
+        return True
+
+    def untagFile(self, tagName):
+        if not self.tmsu.untag(self.fileName, tagName):
+            self.displayError("Failed to untag file.")
+            return False
+        return True
+
+    def loadTags(self):
+        allTags = self.tmsu.tags()
+        fileTags = self.tmsu.tags(self.fileName)
+        for tag in allTags:
+            self.store.append([tag, tag in fileTags])
+
+    def displayError(self, msg):
+        dialog = Gtk.MessageDialog(
+            self, Gtk.DialogFlags.MODAL, Gtk.MessageType.ERROR,
+            Gtk.ButtonsType.CLOSE, msg)
+        dialog.run()
+        dialog.destroy()
 
 if __name__ == "__main__":
     err = None
@@ -89,7 +146,7 @@ if __name__ == "__main__":
     else:
         print(tmsu.info())
         print(tmsu.tags("testfile"))
-        win = MyWindow()
+        win = MyWindow(tmsu, "testfile2")
         win.connect('delete-event', Gtk.main_quit)
         win.show_all()
         Gtk.main()
